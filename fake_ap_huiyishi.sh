1184fc67e175abb1092d362972a645956923bde0
NATA <379718363@qq.com>
2019-03-16 20:19:37 +0800

    rapair fake ap address

--- fake_ap_huiyishi.sh
+++ fake_ap_huiyishi.sh
@@ -5,10 +5,16 @@ nft add chain nat postrouting { type nat hook postrouting priority 0 \;}
 nft add rule nat postrouting oifname "eth0" masquerade
 nmcli dev set wlan0 managed no
 ip link set wlan0 down
-ip link set wlan0 address 46:96:72:AF:EC:3F
+ip link set wlan0 address 34:96:72:AF:EC:3F
+ip link set wlan0 up
 hostapd /etc/hostapd/hostapd.conf
 
 ip addr add 10.5.5.1/24 dev wlan0
 
 dnsmasq -i wlan0 --dhcp-range=10.5.5.100,10.5.5.200,2h
 
+########################################################################
+#可用wifi MAC地址，名字不重要，只检验mac地址
+#NetCore 08:10:77:54:81
+#huiyishi 34:96:72:AF:EC:3F
+#TP-Link_Lx_5G cc:81:da:d7:3d
